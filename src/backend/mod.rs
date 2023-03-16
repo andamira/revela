29c197e2929f3c91dd32a46122dda523e8780390
joseLuís <joseluis@andamira.net>
2023-03-16 13:11:51 +0100

    update CIs

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -35,48 +35,49 @@ pub(crate) mod all {
         TextGridCapabilities, WindowCapabilities,
     };
     // IMPROVE: alloc
+    #[doc(inline)]
     #[cfg(feature = "std")]
     #[cfg_attr(feature = "nightly", doc(cfg(feature = "std")))]
     pub use super::capabilities::SystemCapabilities;
 
     // export the panic handlers and other no-std utilities.
-    #[cfg(feature = "no-std")]
     #[doc(inline)]
+    #[cfg(feature = "no-std")]
     pub use super::no_std::*;
 
     // export the global allocators and other alloc utilities.
-    #[cfg(all(feature = "alloc", feature = "no-std"))]
     #[doc(inline)]
+    #[cfg(all(feature = "alloc", feature = "no-std"))]
     pub use super::alloc::*;
 
     /* terminal */
 
+    #[doc(inline)]
     #[cfg(feature = "crossterm")]
     #[cfg_attr(feature = "nightly", doc(cfg(feature = "crossterm")))]
-    #[doc(inline)]
     pub use super::crossterm::CrosstermBackend;
 
+    #[doc(inline)]
     #[cfg(feature = "notcurses")]
     #[cfg_attr(feature = "nightly", doc(cfg(feature = "notcurses")))]
-    #[doc(inline)]
     pub use super::notcurses::{NotcursesBackend, NotcursesTextGrid};
 
     /* */
 
+    #[doc(inline)]
     #[cfg(feature = "sdl2")]
     #[cfg_attr(feature = "nightly", doc(cfg(feature = "sdl2")))]
-    #[doc(inline)]
     pub use super::sdl2::{Sdl2Backend, Sdl2EventSource};
 
     /* */
 
+    #[doc(inline)]
     #[cfg(feature = "gilrs")]
     #[cfg_attr(feature = "nightly", doc(cfg(feature = "gilrs")))]
-    #[doc(inline)]
     pub use super::gilrs::GilrsBackend;
 
+    #[doc(inline)]
     #[cfg(feature = "midir")]
     #[cfg_attr(feature = "nightly", doc(cfg(feature = "midir")))]
-    #[doc(inline)]
     pub use super::midir::MidirBackend;
 }
