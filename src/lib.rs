7aab7267c281f4d3fee3edc5d475c8bd7b318080
joseLuís <joseluis@andamira.net>
2023-11-18 17:12:39 +0100

    remove deprecated features
    - tiny refactor.

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,10 @@
 // warnings
 #![warn(clippy::all)]
 #![allow(uncommon_codepoints, clippy::module_inception, non_upper_case_globals)]
-// environment
-#![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(feature = "safe", forbid(unsafe_code))]
+// nightly, safety, environment
 #![cfg_attr(feature = "nightly", feature(doc_cfg))]
+#![cfg_attr(feature = "safe", forbid(unsafe_code))]
+#![cfg_attr(not(feature = "std"), no_std)]
 #[cfg(feature = "alloc")]
 extern crate alloc;
 
@@ -23,12 +23,6 @@ compile_error!("You can't enable the `std` and `no_std` features at the same tim
     any(feature = "unsafe", feature = "unsafe_init", feature = "unsafe_libc")
 ))]
 compile_error!("You can't enable the `safe` and `unsafe*` features at the same time.");
-// deprecated
-devela::deprecate_feature![old: "no-std", new: "no_std", since: "0.0.8"];
-devela::deprecate_feature![old: "backends_no-std", new: "all_no_std", since: "0.0.8"];
-devela::deprecate_feature![old: "backends_no_std", new: "all_no_std", since: "0.0.9"];
-devela::deprecate_feature![old: "backends_alloc", new: "all_alloc", since: "0.0.9"];
-devela::deprecate_feature![old: "backends_std", new: "all_std", since: "0.0.9"];
 
 pub mod backend;
 pub mod error;
